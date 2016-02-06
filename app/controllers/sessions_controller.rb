5b85ec73c82d68c484bb3d178d44674b4e5cfd4e
Nathaniel Williams <nathaniel.williams@gmail.com>
2016-02-06 17:17:17 -0500

    user actions complete - edit/index/destroy

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,11 +8,11 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       log_in user
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      redirect_to user
+      redirect_back_or user
     else
       #flashmsg = image_tag("pythonpoint.jpg", alt: "Python Patrol", class: "flashimg")
       #flashmsg = "<img src='" + image_url("pythonpoint.jpg") + "' alt='Python Patrol' class='flashimg' />"
-flashmsg = "<h3>Those credentials aren't legit</h3><br />"
+      flashmsg = "<h3>Those credentials aren't legit</h3><br />"
       flashmsg += "<h4>Lying to the hulkster's a great way to meet the pythons, brother!</h4>"
       flash.now[:danger] = flashmsg.html_safe # Not quite right!
       render 'new'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,12 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:show, :edit, :update, :index, :destroy]
+  before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
   def show
     @user = User.find(params[:id])
   end
@@ -11,11 +19,35 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       log_in @user
-      flash[:success] = "There's no turning back now, brother!"
+      flashmsg = "<h3>Your Profile has been Created!</h3><br />"
+      flashmsg += "<h4>To all my little Hulkamaniacs,<br/>say your prayers,<br/>take your vitamins,<br/>and you just might survive sharing the ring with me.</h4>"
+      flash[:success] = flashmsg
       redirect_to @user
     else
       render 'new'
     end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flashmsg = "<h3>Your Profile has been Updated!</h3><br />"
+      flashmsg += "<h4>This pleases the hulk almost as much as being a champion does.</h4>"
+      flash[:success] = flashmsg
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "<h3>The Weak will be Destroyed, Brother!</h3><h4>The Hulkster's happy to report that this unworthy competitor has been removed from the ring.</h4>"
+    redirect_to users_url
   end
 
   private
@@ -23,5 +55,35 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
+    end
+
+    # Before filters
+
+    # Confirms a logged-in user.
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flashmsg = "<h3>You Need to Login, brother!</h3><br />"
+        flashmsg += "<h4>The thought of some unauthorized chump or chumpette creepin' around the Hulkster's home, invading the privacy of all these Hulkamaniacs...it's riling up these pythons. <br/> If I were you, I'd hightail it outta here while your legs still work.</h4>"
+        flash[:danger] = flashmsg
+        redirect_to login_url
+      end
+    end
+
+    # Confirms the correct user.
+    def correct_user
+      @user = User.find(params[:id])
+      if (@user != current_user)
+        flashmsg = "<h3>That ain't your account, brother!</h3><br />"
+        flashmsg += "<h4>Stick to managing your own profile, or this will end with an atomic leg drop.</h4>"
+        flash[:danger] = flashmsg
+        redirect_to(root_url)
+      end
+
+    end
+
+    # Confirms an admin user.
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
     end
 end
